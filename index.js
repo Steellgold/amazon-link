326b95621da930ab5d0d507cd2ff0aeb2b073b40
Gaëtan H <gaetanhus@gmail.com>
2024-10-18 17:32:20 +0200

    feat: add CORS middleware to Express application

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
